69ed7256d4fa3e11a81f64e9f00515039fb1cfd7
hamayun <hamayun.waheed1999@gmail.com>
2021-02-10 02:00:06 +0500

    fixed rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
   def follow_check(user)
-    if current_user != user
-      followers_array = user.followers
-      followers_array = followers_array.all.pluck(:id)
-      if followers_array.include?(current_user.id)
-        'Followed'
-      else
-        button_to 'Follow', follows_path(followed_id: user.id, follower_id: current_user.id)
-      end
+    return if current_user == user
+
+    followers_array = user.followers
+    followers_array = followers_array.all.pluck(:id)
+    if followers_array.include?(current_user.id)
+      'Followed'
+    else
+      button_to 'Follow', follows_path(followed_id: user.id, follower_id: current_user.id)
     end
   end
 
