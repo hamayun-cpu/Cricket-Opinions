2c038762c828462384795410fbcd763d332840de
hamayun <hamayun.waheed1999@gmail.com>
2021-02-09 15:52:54 +0500

    finalised everything for mvc

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,17 +10,4 @@ module UsersHelper
       end
     end
   end
-
-  def acceptable_main_image
-    return unless main_image.attached?
-
-    unless main_image.byte_size <= 1.megabyte
-      errors.add(:main_image, 'is too big')
-    end
-
-    acceptable_types = ["image/jpeg", "image/png"]
-    unless acceptable_types.include?(main_image.content_type)
-      errors.add(:main_image, "must be a JPEG or PNG")
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,32 @@ class User < ApplicationRecord
   has_one_attached :cover_image
 
   validate :acceptable_main_image
+
+  validate :acceptable_cover_image
+
+  def acceptable_main_image
+    return unless main_image.attached?
+
+    unless main_image.byte_size <= 1.megabyte
+      errors.add(:main_image, 'is too big')
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
+  end
+
+  def acceptable_cover_image
+    return unless cover_image.attached?
+
+    unless cover_image.byte_size <= 1.megabyte
+      errors.add(:cover_image, 'is too big')
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(cover_image.content_type)
+      errors.add(:cover_image, "must be a JPEG or PNG")
+    end
+  end
 end
