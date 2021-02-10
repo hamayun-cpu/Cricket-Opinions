ac8fb0e2ca432ad7d815db7c7c53b5efa2dc137e
hamayun <hamayun.waheed1999@gmail.com>
2021-02-11 02:23:57 +0500

    delete ifs from who-to-follow view

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,2 +1,9 @@
 module OpinionsHelper
+  def profile_pic_rounded(user)
+    if user.main_image.attached?
+      image_tag user.main_image, class: 'profile-pic rounded-circle'
+    else
+      image_tag('profile.png', class: 'rounded-circle', height: 70, width: 70)
+    end
+  end
 end
